7fab5a9d57de010cb72886ad5401182721e44731
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-04-18 12:13:13 +0200

    raw

---
+++ app/01-github-repositories/main-raw.js
@@ -0,0 +1,30 @@
+const https = require('https');
+
+const options = {
+    host: 'api.github.com',
+    port: 443,
+    path: '/users/jlguenego/starred?per_page=100',
+    headers: {
+        'User-Agent': 'Node',
+    },
+    agent: false,
+};
+
+const req = https.get(options, function (res) {
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data', (chunk) => {
+        rawData += chunk;
+    });
+    res.on('end', () => {
+        try {
+            const body = JSON.parse(rawData);
+            console.log('Here is the list of all the repos I have starred:');
+            body.forEach(r => console.log('name: ' + r.full_name));
+        } catch (e) {
+            console.error(e.message);
+        }
+    });
+
+});
+
