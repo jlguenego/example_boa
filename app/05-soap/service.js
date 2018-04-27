e7c9cd979443c009e2e9d5fcfc0ea5b6e8b8d0e3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-04-27 19:48:03 +0200

    ok

---
+++ app/05-soap/service.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+    MessageSplitterService: {
+        MessageSplitterServiceSoapPort: {
+            MessageSplitter: splitter_function
+        },
+        MessageSplitterServiceSoap12Port: {
+            MessageSplitter: splitter_function
+        }
+    }
+};
+
+// the splitter function, used by the service
+function splitter_function(args) {
+    console.log('splitter_function');
+    var splitter = args.splitter;
+    var splitted_msg = args.message.split(splitter);
+    var result = [];
+    for (var i = 0; i < splitted_msg.length; i++) {
+        result.push(splitted_msg[i]);
+    }
+    return {
+        result: result
+    }
+}
