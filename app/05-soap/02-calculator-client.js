b88effe0361e7fe0cc59b30a5e4669910b18ab4f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-04-29 17:12:29 +0200

    ok working

--- app/05-soap/02-calculator-client.js
+++ app/05-soap/02-calculator-client.js
@@ -2,30 +2,13 @@ var soap = require('soap');
 var url = 'http://localhost:8000/wsdl?wsdl';
 
 // Create client
-soap.createClient(url, function (err, client) {
+soap.createClient(url, async function (err, client) {
     if (err) {
         throw err;
     }
-    /* 
-    * Parameters of the service call: they need to be called as specified
-    * in the WSDL file
-    */
-    var args = {
-        a: "3",
-        b: "5"
-    };
-    // call the service
-    client.Substract(args, function (err, res, rawResponse, soapHeader, rawRequest) {
-        if (err)
-            throw err;
-        // print the service returned result
-        console.log(res);
-        client.Add(args, function (err, res, rawResponse, soapHeader, rawRequest) {
-            if (err)
-                throw err;
-            // print the service returned result
-            console.log(res);
-    
-        });
-    });
+
+    const [add] = await client.AddAsync({ a: "3", b: "5" });
+    console.log(add.result);
+    const [substract] = await client.SubstractAsync({ a: "3", b: "5" });
+    console.log(substract.result);
 });
