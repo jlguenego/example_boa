df1773a6fe4c5699241169dfd9502d7610b12d6c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-05-01 14:47:07 +0200

    ok

--- app/07-swagger/src/index.js
+++ app/07-swagger/src/index.js
@@ -2,7 +2,7 @@ import 'angular/angular-csp.css';
 import './style.css';
 import 'angular';
 
-import { OneTicketApi, ApiClient } from '../javascript-client-generated/src/index';
+import { OneTicketApi, AllTicketsApi, ApiClient } from '../javascript-client-generated/src/index';
 
 const app = angular.module('myApp', []);
 
@@ -10,90 +10,100 @@ const url = '../../../ws/tickets';
 ApiClient.instance.basePath = '../../../ws';
 
 const oneTicketApi = new OneTicketApi();
+const allTicketApi = new AllTicketsApi();
 
-function MyController($http) {
+function MyController($scope, $http) {
+
+    this.query = function () {
+        console.log('appel query en cours...');
+        // return $http.get(url)
+        //     .then(response => this.tickets = response.data.content)
+        //     .catch(e => console.log('error', e));
+        return allTicketApi.retrieveAllTickets()
+            .then(response => {
+                console.log('response', response);
+                this.tickets = response.content;
+                $scope.$apply();
+            })
+            .catch(e => console.log('error', e));
 
 
+    };
 
-	this.query = function () {
-		console.log('appel query en cours...');
-		return $http.get(url).then(response => this.tickets = response.data.content).catch(e => console.log('error', e));
-	};
-
-	this.create = function () {
+    this.create = function () {
         console.log('appel create en cours...', OneTicketApi);
         return oneTicketApi.createTicket({
             ticket: this.newTicket
         }).then(() => {
-			this.query();
-		}).catch((error) => {
-			console.error('error', error);
-		});
-	};
+            this.query();
+        }).catch((error) => {
+            console.error('error', error);
+        });
+    };
 
-	this.empty = function () {
-		console.log('appel delete all en cours...');
-		return $http.delete(url).then(() => {
-			this.query();
-		}).catch((error) => {
-			console.error('error', error);
-		});
-	};
+    this.empty = function () {
+        console.log('appel delete all en cours...');
+        return $http.delete(url).then(() => {
+            this.query();
+        }).catch((error) => {
+            console.error('error', error);
+        });
+    };
 
-	this.retrieve = function (id) {
-		console.log('appel retrieve en cours...', id);
-		return $http.get(`${url}/${id}`).then((response) => {
-			console.log('ticket', response.data.content);
-			this.retrievedTicket = response.data.content;
-		}).catch((error) => {
-			console.error('error', error);
-		});
-	};
+    this.retrieve = function (id) {
+        console.log('appel retrieve en cours...', id);
+        return $http.get(`${url}/${id}`).then((response) => {
+            console.log('ticket', response.data.content);
+            this.retrievedTicket = response.data.content;
+        }).catch((error) => {
+            console.error('error', error);
+        });
+    };
 
-	this.update = function (ticket) {
-		console.log('appel update en cours...');
-		return $http.put(`${url}/${ticket._id}`, {
-			_id: ticket._id,
-			name: ticket.newName
-		}).then((response) => {
-			console.log('ticket', response.data.content);
-			this.ticket = response.data.content;
-			this.query();
-		}).catch((error) => {
-			console.error('error', error);
-		});
-	}
+    this.update = function (ticket) {
+        console.log('appel update en cours...');
+        return $http.put(`${url}/${ticket._id}`, {
+            _id: ticket._id,
+            name: ticket.newName
+        }).then((response) => {
+            console.log('ticket', response.data.content);
+            this.ticket = response.data.content;
+            this.query();
+        }).catch((error) => {
+            console.error('error', error);
+        });
+    }
 
-	this.delete = function (ticket) {
-		console.log('appel delete en cours...');
-		return $http.delete(`${url}/${ticket._id}`).then((response) => {
-			console.log('ticket', response.data.content);
-			this.ticket = response.data.content;
-			this.query();
-		}).catch((error) => {
-			console.error('error', error);
-		});
-	}
+    this.delete = function (ticket) {
+        console.log('appel delete en cours...');
+        return $http.delete(`${url}/${ticket._id}`).then((response) => {
+            console.log('ticket', response.data.content);
+            this.ticket = response.data.content;
+            this.query();
+        }).catch((error) => {
+            console.error('error', error);
+        });
+    }
 
-	this.query();
+    this.query();
 }
 
 
-app.controller('MyController', ['$http', MyController]);
+app.controller('MyController', ['$scope', '$http', MyController]);
 
 app.directive('jlgClickAndDisable', function () {
-	return {
-		scope: {
-			jlgClickAndDisable: '&'
-		},
-		controller: ['$scope', '$element', function ($scope, $element) {
-			$element.bind('click', function () {
-				console.log('disable the button');
-				$element.prop('disabled', true);
-				$scope.jlgClickAndDisable().finally(function () {
-					$element.prop('disabled', false);
-				})
-			});
-		}]
-	};
+    return {
+        scope: {
+            jlgClickAndDisable: '&'
+        },
+        controller: ['$scope', '$element', function ($scope, $element) {
+            $element.bind('click', function () {
+                console.log('disable the button');
+                $element.prop('disabled', true);
+                $scope.jlgClickAndDisable().finally(function () {
+                    $element.prop('disabled', false);
+                })
+            });
+        }]
+    };
 });
