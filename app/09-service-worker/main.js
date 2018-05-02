60d3efcf38f3f7c2ed052f6fe8fe4a3b54758c46
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-05-02 10:43:25 +0200

    ok

---
+++ app/09-service-worker/main.js
@@ -0,0 +1,18 @@
+function register() {
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function () {
+            console.log('window loaded');
+            navigator.serviceWorker.register('./sw.js').then(function (registration) {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, function (err) {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        });
+    }
+}
+
+register();
+console.log('script loaded');
+
