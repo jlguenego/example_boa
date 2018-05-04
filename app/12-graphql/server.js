43fdbb0ee67b03195ec782c9b58c8df17f00dd2c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-05-04 08:26:59 +0200

    ok

--- app/12-graphql/server.js
+++ app/12-graphql/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const serveIndex = require('serve-index');
+const ws = require('./ws');
+
 
 const app = express();
+
+app.use('/ws', ws);
 
 app.use(express.static('../../'));
 app.use(serveIndex('../../', { icons: true }));
 
-app.listen(8000, function() {
-	console.log('HTTP server started on port 8000');
+app.listen(8000, function () {
+    console.log('HTTP server started on port 8000');
 });
