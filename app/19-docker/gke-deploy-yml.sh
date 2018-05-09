6e86f814e473f26d6b903bb92f6c8a73e7c09aab
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-05-09 17:17:54 +0200

    ok

--- app/19-docker/gke-deploy-yml.sh
+++ app/19-docker/gke-deploy-yml.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -eaux
 
-
-export GCR_HOSTNAME=eu.gcr.io
-export PROJECT_ID="$(gcloud config get-value project -q)"
-
-kubectl apply -f gke-deploy.yml
+kubectl create -f yml/deployment.yml
+kubectl create -f yml/service.yml
