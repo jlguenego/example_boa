beb278aaf6e24f3a58d83d267ffa5ed58a4c15fc
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-05-09 12:03:55 +0200

    ok

--- app/19-docker/kb-cleanup.sh
+++ app/19-docker/kb-cleanup.sh
@@ -7,4 +7,7 @@ sleep 10
 gcloud compute forwarding-rules list
 
 # Wait that the list send back 0 items to run the following.
-gcloud container clusters delete hello-cluster  --zone=europe-west4-a --async
+gcloud container clusters delete hello-cluster  --zone=europe-west4-a --async <<EOF
+Y
+EOF
+
